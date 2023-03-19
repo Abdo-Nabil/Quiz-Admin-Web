e150e30ed29d08c388413d9a7fa1918751cb7faa
Abdo Nabil <abdo.sw.eng@gmail.com>
2023-03-19 17:55:09 +0200

    edit default num of questions

--- lib/resources/constants_manager.dart
+++ lib/resources/constants_manager.dart
@@ -1,5 +1,5 @@
 class ConstantsManager {
   static const double containerMaxWidth = 1250;
-  static const int defaultQuestionNumbers = 2;
+  static const int defaultQuestionNumbers = 1;
   static const int defaultChoicesNumbers = 4;
 }
